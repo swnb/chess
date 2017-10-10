15af37c10a351f7fd5569dd5c943c7b1e2b9ff42
swnb <1786718956@qq.com>
2017-10-10 12:21:37 +0800

    重构代码

---
+++ app/components/container.jsx
@@ -0,0 +1,59 @@
+import Docker from 'com/docker';
+import React from 'react';
+
+class Show extends React.Component {}
+// ReactDom.render(
+// <Docker size="81" winCount="5" />,
+// document.querySelector('.container')
+// );
+
+class Container extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            render: false,
+            size: 0,
+            winCount: 5
+        };
+        this.initGame = this.initGame.bind(this);
+    }
+    initGame() {
+        if (this.refs.size.value && this.refs.winCount.value) {
+            const size = parseInt(this.refs.size.value);
+            const winCount = parseInt(this.refs.winCount.value);
+            if (isNaN(size) || isNaN(winCount)) {
+                alert('需要数字!来表示棋盘的大小');
+            } else {
+                this.setState({
+                    size: Math.pow(size, 2),
+                    winCount: winCount,
+                    render: true
+                });
+            }
+        } else {
+            alert('需要数字作为棋盘的大小');
+        }
+    }
+    render() {
+        if (!this.state.render) {
+            return (
+                <form action="">
+                    <fieldset>
+                        <legend>输入你的希望的棋盘数目</legend>
+                        <h3>输入你想需要的棋盘大小</h3>
+                        <input type="text" ref="size" />
+                        <h3>输入判断输赢的数目</h3>
+                        <input type="text" ref="winCount" placeholder="5" />
+                        <button onClick={this.initGame}>开始棋盘游戏</button>
+                    </fieldset>
+                </form>
+            );
+        } else {
+            return (
+                <Docker size={this.state.size} winCount={this.state.winCount} />
+            );
+        }
+    }
+}
+
+export default Container;
