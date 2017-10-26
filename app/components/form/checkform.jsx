75193f48a6254d823a7b92c1f87dbcbbfa2b388b
swnb <1786718956@qq.com>
2017-10-26 12:49:49 +0800

    表单更换称为antd的风格

---
+++ app/components/form/checkform.jsx
@@ -0,0 +1,130 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'src/form.css';
+import { Form, Input, Slider, Switch, Button } from 'antd';
+const FormItem = Form.Item;
+
+class CheckBoardForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            formLayout: 'vertical',
+            info: '定义棋盘',
+            passwordRequired: false
+        };
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handlePasswdChange = this.handlePasswdChange.bind(this);
+    }
+    handlePasswdChange(value) {
+        this.setState({
+            passwordRequired: value
+        });
+    }
+    handleSubmit(e) {
+        e.preventDefault();
+        const values = this.props.form.getFieldsValue();
+        this.props.initGame(values);
+    }
+    render() {
+        const { formLayout } = this.state;
+        const { getFieldDecorator } = this.props.form;
+        const formItemLayout =
+            null &&
+            {
+                // labelCol: { span: 6 },
+                // wrapperCol: { span: 14 }
+            };
+
+        return (
+            <div className="form">
+                <Form onSubmit={this.handleSubmit} layout={formLayout}>
+                    <FormItem {...formItemLayout} label="自定义棋盘房间名称">
+                        {getFieldDecorator('roomId', {
+                            rules: [
+                                {
+                                    required: true,
+                                    message: '输入你的房间名称,名称的最大长度是10',
+                                    transform: e => e.trim(),
+                                    max: 10
+                                }
+                            ]
+                        })(<Input size="large" placeholder="房间名称" />)}
+                    </FormItem>
+
+                    <FormItem {...formItemLayout} label="是否需要密码呢?">
+                        <Switch
+                            checkedChildren="开"
+                            unCheckedChildren="关"
+                            onChange={this.handlePasswdChange}
+                        />
+                        {getFieldDecorator('passwd', { initialValue: null })(
+                            <Input
+                                size="large"
+                                placeholder="输入密码，可以不填写"
+                                disabled={!this.state.passwordRequired}
+                            />
+                        )}
+                    </FormItem>
+
+                    <FormItem {...formItemLayout} label="选择你自己的棋盘尺寸 (n X n)">
+                        {getFieldDecorator('size', {
+                            initialValue: 30
+                        })(
+                            <Slider
+                                min={3}
+                                max={100}
+                                marks={{
+                                    3: '3 X 3',
+                                    10: '10 X 10',
+                                    20: '20 X 20',
+                                    30: '30 X 30',
+                                    40: '40 X 40',
+                                    50: '50 X 50',
+                                    60: '60 X 60',
+                                    100: {
+                                        style: {
+                                            color: '#f50'
+                                        },
+                                        label: <strong>100 X 100</strong>
+                                    }
+                                }}
+                                included={false}
+                            />
+                        )}
+                    </FormItem>
+
+                    <FormItem {...formItemLayout} label="判断输赢的棋子相连数目">
+                        {getFieldDecorator('winCount', {
+                            initialValue: 5
+                        })(
+                            <Slider
+                                min={3}
+                                max={20}
+                                included={false}
+                                marks={{
+                                    5: '5子棋',
+                                    7: '7子棋',
+                                    10: '10子棋',
+                                    20: {
+                                        style: {
+                                            color: '#f50'
+                                        },
+                                        label: <strong>20子棋</strong>
+                                    }
+                                }}
+                            />
+                        )}
+                    </FormItem>
+
+                    <FormItem wrapperCol={{ span: 12, offset: 6 }}>
+                        <Button type="primary" htmlType="submit">
+                            Submit
+                        </Button>
+                    </FormItem>
+                </Form>
+            </div>
+        );
+    }
+}
+
+export default Form.create()(CheckBoardForm);
--- app/components/init-checkboard/initplayer.jsx
+++ app/components/init-checkboard/initplayer.jsx
@@ -3,8 +3,8 @@ import HistoryList from 'base/historylist';
 import { NetCheckerBoarder, store } from 'com/winner/net';
 import { Checkerplayer } from 'com/winner/sigel';
 import React from 'react';
+import CheckerForm from 'com/form/checkform';
 
-// import {Form,Icon,Buttom} from 'antd'
 class InitPage extends React.Component {
     constructor(props) {
         super(props);
@@ -17,41 +17,29 @@ class InitPage extends React.Component {
         };
         this.initGame = this.initGame.bind(this);
     }
-    initGame() {
-        if (this.refs.size.value && this.refs.roomid.value) {
-            const [size, winCount, roomid] = [
-                parseInt(this.refs.size.value),
-                parseInt(this.refs.winCount.value || 5),
-                this.refs.roomid.value
-            ];
-            if (isNaN(size) || isNaN(winCount)) {
-                alert('需要数字!来表示棋盘的大小或者输赢规则');
-            } else {
-                const hocks = {
-                    next: () => {
-                        alert('你需要等到下一个人进入房间和你玩耍,在这之前,你可以熟悉规则');
-                        console.log('room made');
-                        this.setState({
-                            size: Math.pow(size, 2),
-                            winCount: winCount,
-                            render: true
-                        });
-                    },
-                    err() {
-                        alert('请更新一下房间名看看吧');
-                    },
-                    intoRoom: data => {
-                        this.data = data;
-                        this.setState({
-                            netChecker: true
-                        });
-                    }
-                };
-                makeRoom(roomid, size, winCount, hocks);
+    initGame(values) {
+        const { roomId, passwd, size, winCount } = values;
+        const hooks = {
+            next: () => {
+                alert('你需要等到下一个人进入房间和你玩耍,在这之前,你可以熟悉规则');
+                console.log('room made');
+                this.setState({
+                    size: Math.pow(size, 2),
+                    winCount: winCount,
+                    render: true
+                });
+            },
+            err() {
+                alert('请更新一下房间名看看吧');
+            },
+            intoRoom: data => {
+                this.data = data;
+                this.setState({
+                    netChecker: true
+                });
             }
-        } else {
-            alert('需要全部填写');
-        }
+        };
+        makeRoom(roomId, size, winCount, hooks);
     }
     componentDidMount() {
         //相应状态
@@ -64,30 +52,7 @@ class InitPage extends React.Component {
     }
     render() {
         if (!this.state.render) {
-            return (
-                <fieldset>
-                    <legend>输入你的棋盘和期望</legend>
-                    <h3>棋盘大小</h3>
-                    <input type="text" ref="size" required />
-                    <span>
-                        <h4>n x n的棋盘 输入 n</h4>
-                    </span>
-                    <h3>输入判断输赢的数目</h3>
-                    <input
-                        type="text"
-                        ref="winCount"
-                        placeholder="5"
-                        required
-                    />
-                    <span>
-                        <h4>5子相连胜出输入5</h4>
-                    </span>
-                    <h3>为了公平,选边是系统随机决定</h3>
-                    <h3>设置你的roomid</h3>
-                    <input type="text" ref="roomid" required />
-                    <button onClick={this.initGame}>开始棋盘游戏</button>
-                </fieldset>
-            );
+            return <CheckerForm initGame={this.initGame} />;
         } else {
             if (this.state.netChecker) {
                 const { size, winCount, roomId, room_uuid, side } = this.data;
