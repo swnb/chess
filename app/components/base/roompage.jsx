5d3033e6b7bad05838cd4dff73849f900099d2a1
swnb <1786718956@qq.com>
2017-10-28 00:30:53 +0800

    主页修改ui

--- app/components/base/roompage.jsx
+++ app/components/base/roompage.jsx
@@ -2,33 +2,69 @@ import 'src/homepage.css';
 
 import React from 'react';
 import Passwd from 'com/form/passwd';
+import { Card, Col, Row } from 'antd';
+function arr_sort(arr, num = 3) {
+    let arr_tmp = [];
+    function sort(arr) {
+        if (arr.length >= num) {
+            let arr_num = [];
+            for (var i = 0; i < num; i++) {
+                arr_num.push(arr.pop());
+            }
+            arr_tmp.push(arr_num);
+            sort(arr);
+        } else {
+            arr_tmp.push(arr);
+        }
+    }
+    sort(arr);
+    return arr_tmp;
+}
+
 function RoomPage(props) {
     let roomList = props.room.map((e, i) => {
         const title = `房间名称 ${e.id}`;
-        const info = `棋盘大小${e.module.size}  ${e.module.winCount}子棋`;
-        const div = (
-            <div className="info" key={i}>
-                <article>{title}</article>
-                {info}
-                <Passwd
-                    roomId={e.id}
-                    getIntoRoom={() => {
-                        props.getIntoRoom(e.id);
-                    }}
-                />
-            </div>
+        const dom = (
+            <Col span={8} key={i}>
+                <Card
+                    title={title}
+                    extra={
+                        <Passwd
+                            roomId={e.id}
+                            getIntoRoom={() => {
+                                props.getIntoRoom(e.id);
+                            }}
+                        />
+                    }
+                    style={{ maxWidth: 300 }}
+                >
+                    <p>{`棋盘大小${e.module.size}`}</p>
+                    <p>{`${e.module.winCount}子棋`}</p>
+                </Card>
+            </Col>
         );
-        return div;
+        return dom;
     });
     roomList.push(
-        <div
-            className="createRoom"
-            onClick={props.onClick}
-            key={props.room.length}
-        >
-            自定义你的房间
+        <Col span={8} key={props.room.length}>
+            <Card
+                extra={
+                    <a href="#" onClick={props.onClick}>
+                        创建房间
+                    </a>
+                }
+                title={'自己定制房间'}
+                style={{ maxWidth: 300 }}
+            >
+                <p>棋盘大小自定义</p>
+                <p>输赢判断自定义</p>
+            </Card>
+        </Col>
+    );
+    return (
+        <div className="roomList">
+            <Row gutter={16}>{roomList}</Row>
         </div>
     );
-    return <div className="roomlist">{roomList}</div>;
 }
 export default RoomPage;
