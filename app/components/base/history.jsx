4a87eeef8f995a786041c9831b4e9a1be4348a7a
swnb <1786718956@qq.com>
2018-03-10 21:40:05 +0800

    add special color

--- app/components/base/history.jsx
+++ app/components/base/history.jsx
@@ -4,7 +4,12 @@ import Checkerboard from 'base/checkerboard';
 import React from 'react';
 
 function CheckerHistory(props) {
+    const specialColor = 'orange'
     const arr = props.pos.map((e, i) => {
+        if (e !== null && typeof e === 'object' && e.special) {
+            console.log(e.value)
+            return <button key={i} style={{ backgroundColor: specialColor }}>{e.value}</button>
+        }
         return <button key={i}>{e}</button>;
     });
     return (
