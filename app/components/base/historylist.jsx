da314a51e70c1587c53ea5a2a7de8a382737e4c3
swnb <1786718956@qq.com>
2018-03-11 21:16:18 +0800

    add antd

--- app/components/base/historylist.jsx
+++ app/components/base/historylist.jsx
@@ -1,14 +1,40 @@
-import 'src/index.css';
-
+import 'src/history.css';
 import CheckerHistory from 'base/history';
 import React from 'react';
+
+import { Tabs } from 'antd';
+
+const TabPane = Tabs.TabPane;
+
+
 
 function HistoryList(props) {
     const size = Math.sqrt(props.size);
     const list = props.pos.filter(e => e).map((e, i) => {
-        return <CheckerHistory key={i} size={size} pos={e} />;
+        return <TabPane tab={`Tab ${i}`} key={i}>
+            <CheckerHistory
+                size={
+                    size
+                }
+                pos={
+                    e
+                }
+            />
+        </TabPane>
     });
-    return <div className="list">{list}</div>;
+
+    return <div style={{ marginTop: "10px" }}>
+        <Tabs
+            defaultActiveKey="1"
+            tabPosition="left"
+            style={{ height: 600 }}
+        >
+            {list}
+        </Tabs>
+    </div>
 }
 
 export default HistoryList;
+
+
+
