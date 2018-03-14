2ea87fb8289844c3de8883e49bbbfe4538aa9650
swnb <1786718956@qq.com>
2018-03-14 21:08:07 +0800

    移除显示密码

--- app/api/passwdconfirm.js
+++ app/api/passwdconfirm.js
@@ -1,9 +1,14 @@
 import io from 'socket.io-client';
 //{err(),success(),errRoom()}
-const passwdComfirm = ({ roomId, passwd }, hooks) => {
+const passwdComfirm = ({
+    roomId,
+    passwd
+}, hooks) => {
     const socket = io('/passwdconfirm');
-    console.log(roomId, passwd);
-    socket.emit('passwd confirm', { roomId, passwd });
+    socket.emit('passwd confirm', {
+        roomId,
+        passwd
+    });
     socket.on('err passwd', () => {
         hooks.err();
     });
