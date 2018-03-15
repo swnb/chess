fb5990daefce1e38e5de065ed093ea56ecdd5cd7
swnb <1786718956@qq.com>
2018-03-15 18:07:57 +0800

    fix code style

--- config.js
+++ config.js
@@ -1,18 +1,19 @@
-const path = require('path')
+const path = require('path');
+
 module.exports = {
-    port: 8080,
-    socketioConfig: {
-        // path: 'dist',
-        serveClient: false,
-        //如果成为polling的话,超时连接.1
-        pingTimeout: 120000,
-        pingInterval: 10000,
-        transports: ['polling', 'websocket'],
-        cookie: true
-    },
-    maxRoom: 10,
-    path: {
-        bin: path.resolve(__dirname, 'bin'),
-        api: path.resolve(__dirname, 'api')
-    }
+  port: 8080,
+  socketioConfig: {
+    // path: 'dist',
+    serveClient: false,
+    // 如果成为polling的话,超时连接.1
+    pingTimeout: 120000,
+    pingInterval: 10000,
+    transports: ['polling', 'websocket'],
+    cookie: true,
+  },
+  maxRoom: 10,
+  path: {
+    bin: path.resolve(__dirname, 'bin'),
+    api: path.resolve(__dirname, 'api'),
+  },
 };
