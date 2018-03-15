04883420cdd950eec78f03776cb2603d6ac5336f
swnb <1786718956@qq.com>
2018-03-15 18:09:12 +0800

    server file

--- server.js
+++ server.js
@@ -2,37 +2,46 @@ const fs = require('fs');
 const util = require('util');
 const path = require('path');
 
-const config = require(__dirname + '/config');
-const websocket = require(__dirname + '/controllers/websocket/websocket');
+const config = require(`${__dirname}/config`);
+const websocket = require(`${__dirname}/controllers/websocket/websocket`);
 
 const Koa = require('koa');
-const static = require('koa-static');
+const staticFile = require('koa-static');
 const router = require('koa-router')();
+
 const app = new Koa();
 
 const fsReaddir = util.promisify(fs.readdir);
 
-const main = async ctx => {
-    const contrPath = path.resolve(__dirname + '/controllers/routers/');
-    const files = await fsReaddir(contrPath);
-    // 注册路由
-    files.forEach(file => {
-        const filePath = path.join(contrPath, file);
-        const { urlPath, get, post } = require(filePath);
-        get ? router.get(urlPath, get) : null;
-        post ? router.post(urlPath, post) : null;
-    });
+const main = async () => {
+  const contrPath = path.resolve(`${__dirname}/controllers/routers/`);
+  const files = await fsReaddir(contrPath);
+  // 注册路由
+  files.forEach((file) => {
+    const filePath = path.join(contrPath, file);
+    const {
+      urlPath,
+      get,
+      post,
+    } = require(filePath);
+    if (get) {
+      router.get(urlPath, get);
+    }
+    if (post) {
+      router.post(urlPath, post);
+    }
+  });
 
-    //静态server
-    const staticServer = static(path.join(__dirname, 'dist'));
+  // 静态server
+  const staticServer = staticFile(path.join(__dirname, 'dist'));
 
-    app.use(router.routes());
-    app.use(staticServer);
+  app.use(router.routes());
+  app.use(staticServer);
 
-    const httpServer = app.listen(config.port);
+  const httpServer = app.listen(config.port);
 
-    websocket(httpServer);
-    console.log(`server is running at port ${config.port}`);
+  websocket(httpServer);
+  console.log(`server is running at port ${config.port}`);
 };
 
 module.exports = main;
