303adf57a530494cca86fef7be905dca6b8d718b
swnb <1786718956@qq.com>
2018-03-16 21:54:04 +0800

    resolve

--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,59 @@
 const path = require('path');
 const Myplugin = require('swnb-webpack-learning-plugin');
-const html_templat_path = path.resolve('./template/template.html');
-const resolve = path.resolve;
+
+const htmlTmp = path.resolve('./template/template.html');
+const {
+  resolve,
+} = path;
 module.exports = {
-    entry: {
-        index: path.join(__dirname, 'app', 'index'),
-        // markdown: path.join(__dirname, 'app', 'markdown')
-        test: resolve(__dirname, 'app', 'test')
+  entry: {
+    index: path.join(__dirname, 'app', 'index'),
+    // markdown: path.join(__dirname, 'app', 'markdown')
+    test: resolve(__dirname, 'app', 'test'),
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '',
+  },
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      include: [path.resolve(__dirname, 'app')],
+      exclude: [
+        path.resolve(__dirname, 'node_modules'),
+        path.resolve(__dirname, 'bower_components'),
+      ],
+      loader: 'babel-loader',
+      query: {
+        presets: ['env', 'react'],
+      },
     },
-    output: {
-        filename: '[name].js',
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: ''
+    {
+      test: /\.css$/,
+      include: [path.resolve(__dirname, 'app')],
+      exclude: [path.resolve(__dirname, 'node_modules')],
+      loader: ['style-loader', 'css-loader'],
     },
-    module: {
-        rules: [{
-                test: /\.jsx?$/,
-                include: [path.resolve(__dirname, 'app')],
-                exclude: [
-                    path.resolve(__dirname, 'node_modules'),
-                    path.resolve(__dirname, 'bower_components')
-                ],
-                loader: 'babel-loader',
-                query: {
-                    presets: ['env', 'react']
-                }
-            },
-            {
-                test: /\.css$/,
-                include: [path.resolve(__dirname, 'app')],
-                exclude: [path.resolve(__dirname, 'node_modules')],
-                loader: ['style-loader', 'css-loader']
-            }
-        ]
+    ],
+  },
+  resolve: {
+    extensions: ['.json', '.js', '.jsx', '.css'],
+    alias: {
+      com: resolve('./app/components'),
+      base: resolve('./app/components/base'),
+      src: resolve('./app/src/css'),
+      util: resolve('./app/util'),
+      store: resolve('./app/redux'),
+      api: resolve('./app/api'),
     },
-    resolve: {
-        extensions: ['.json', '.js', '.jsx', '.css'],
-        alias: {
-            com: resolve('./app/components'),
-            base: resolve('./app/components/base'),
-            src: resolve('./app/src/css'),
-            util: resolve('./app/util'),
-            store: resolve('./app/redux'),
-            api: resolve('./app/api')
-        }
-    },
-    devtool: 'source-map',
-    devServer: {
-        contentBase: path.join('./dist'),
-        port: "8000"
-    },
-    plugins: [new Myplugin(html_templat_path, {
-        title: 'playRoom'
-    })]
+  },
+  devtool: 'source-map',
+  devServer: {
+    contentBase: path.join('./dist'),
+    port: '8000',
+  },
+  plugins: [new Myplugin(htmlTmp, {
+    title: 'playRoom',
+  })],
 };
