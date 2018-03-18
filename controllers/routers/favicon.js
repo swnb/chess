9541cfaee282c8cdd360ab85973346f98e7626f0
swnb <1786718956@qq.com>
2018-03-18 19:55:21 +0800

    icon

--- controllers/routers/favicon.js
+++ controllers/routers/favicon.js
@@ -1,14 +1,14 @@
 const fs = require('fs');
 const path = require('path');
 
-const viewsPath = path.join(__dirname, '..', '/dist/index.html');
+const viewsPath = path.join(__dirname, '..', '..', 'favicon.ico');
 const func = async (ctx) => {
   ctx.status = 200;
-  ctx.set('content-type', 'text/html');
+  ctx.set('content-type', 'image/x-icon');
   ctx.body = fs.createReadStream(viewsPath);
 };
 
 module.exports = {
-  urlPath: '/index.server',
+  urlPath: '/favicon.ico',
   get: func,
 };
